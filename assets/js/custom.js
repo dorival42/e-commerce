c1b9cfb48920d06a269e830b8f94bef448978ea6
dorival42 <pdorival181@gmail.com>
2022-09-19 20:11:51 -0400

    Modification custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,3 +14,17 @@ const modalCloseFunc = function() { modal.classList.add('closed')}
 
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
 modalCloseBtn.addEventListener('click',  modalCloseFunc);
+
+//Notification toast variables
+
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+//Notification Function 
+const notificationToastFunc = function() {
+    notificationToast.classList.add('closed')};
+
+// Notification toast evenlistener
+
+toastCloseBtn.addEventListener('click', notificationToastFunc);
+
